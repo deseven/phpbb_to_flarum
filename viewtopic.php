ece5da3a8a8cf180388ff63ec8d6263e7aadc2c7
deseven <de7@deseven.info>
2016-10-23 16:39:51 +0200

    Create viewtopic.php

---
+++ viewtopic.php
@@ -0,0 +1,24 @@
+<?php
+
+$base = "http://forum.example.com";
+$loc = "$base";
+
+if (isset($_GET["t"])) {
+	$loc = "$base/d/{$_GET["t"]}";
+} elseif (isset($_GET["p"])) {
+	$conn = new mysqli("localhost","user","password","phpbb");
+	$conn->set_charset("utf8");
+	$post_id = preg_replace('/\D/','',$_GET["p"]);
+	$res = $conn->query("select topic_id from phpbb_posts where post_id = '$post_id'");
+	if ($res->num_rows) {
+		$post = $res->fetch_assoc();
+		if (isset($post["topic_id"])) {
+			$loc = "$base/d/{$post["topic_id"]}";
+		}
+	}
+	$conn->close();
+}
+
+header("Location: $loc");
+
+?>
